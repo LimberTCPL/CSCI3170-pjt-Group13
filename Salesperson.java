929819a1c6bbce6b1081246bf5762f89959bf8c3
DuncanPoon <130229598+DuncanPoon@users.noreply.github.com>
2023-11-25 17:37:56 +0800

    Update Salesperson.java

--- Salesperson.java
+++ Salesperson.java
@@ -7,6 +7,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 
 import java.util.Scanner;
 //import java.util.Date;
@@ -14,45 +20,36 @@ import java.util.Scanner;
 import java.util.Scanner;
 
 
-public class Salesperson {
+public class sales {
  //Choice: Main Menu move
  //A_Choice_1:Administrator move 1
  //S_Choice_1:Salesperdon move 1   
     
     public static String dbAddress = "jdbc:mysql://projgw.cse.cuhk.edu.hk:2633/db13?autoReconnect=true&useSSL=false";
-        public static String dbUsername = "Group13";
-        public static String dbPassword = "CSCI3170";
+    public static String dbUsername = "Group13";
+    public static String dbPassword = "CSCI3170";
         
         
 
-        public static Connection connectToMySQL() throws IOException{
-                Connection con = null;
-                try{
-                        Class.forName("com.mysql.cj.jdbc.Driver");
-                        con = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
-                        
-                } catch (ClassNotFoundException e){
-                        System.out.println("[Error]: Java MySQL DB Driver not found!!");
-                        System.exit(0);
-                } catch (SQLException e){
-                        System.out.println(e);
-                }
-                return con;
+    public static Connection connectToMySQL() throws IOException{
+        Connection con = null;
+        try{
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                con = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
+                
+        } catch (ClassNotFoundException e){
+                System.out.println("[Error]: Java MySQL DB Driver not found!!");
+                System.exit(0);
+        } catch (SQLException e){
+                System.out.println(e);
         }
-            
-        public static void main(String[] args) {
-            try {
-                Connection conn = connectToMySQL();
-                salesperson_operation(conn);
-            } catch (IOException e) {
-                System.out.println("An error occurred while connecting to the database: " + e.getMessage());
-            } catch (SQLException e) {
-                System.out.println("An SQL error occurred: " + e.getMessage());
-            }
-        }
-
-    public static void salesperson_operation(Connection conn) throws SQLException  {
-        int input;
+        return con;
+    }
+        
+    public static void main(String[] args) throws IOException {
+        Connection conn = connectToMySQL();
+        int input, sale=1;
+        while(sale==1){
         System.out.println("\n-----Operations for salesperson menu-----");
         System.out.println("What kind of operation would you like to perform? ");
         System.out.println("1. Search for parts");
@@ -60,7 +57,7 @@ public class Salesperson {
         System.out.println("3. Return to the main menu:");
         Scanner scan = new Scanner(System.in);
         do {
-            System.out.print("Enter Your Choice: ");
+            System.out.printf("Enter Your Choice: ");
             input = scan.nextInt();
         } while (input < 1 || input > 3);
         if (input == 1)
@@ -68,9 +65,10 @@ public class Salesperson {
         else if (input == 2)
             partselling(conn);
         else if (input == 3)
-            salesperson_operation(conn);
+           // mainmenu.main(args);
+           System.out.println("Hello");
+        }
 
-        scan.close();    
     }
 
     public static void partsearching(Connection conn)   {
@@ -87,13 +85,13 @@ public class Salesperson {
             p_input = scan.nextInt();
         } while (p_input < 1 || p_input > 2);
         scan.nextLine(); // Consume the newline character
-        System.out.println("Type in the Search Keyword:");
+        System.out.printf("Type in the Search Keyword:");
         keyword = scan.nextLine();
         System.out.println("Choose ordering:");
         System.out.println("1. By price, ascending order");
         System.out.println("2. By price, descending order");
         do {
-            System.out.print("Enter Your Choice: ");
+            System.out.printf("Enter Your Choice: ");
             order = scan.nextInt();
         } while (order < 1 || order > 2);
         
@@ -171,6 +169,8 @@ public class Salesperson {
             exp.printStackTrace();
         }
         scan.close();
+        System.out.println("End of Query.\n\n\n");
+        System.out.println("Return to salesperson menu.\n\n\n");
     }
 
 
@@ -178,10 +178,10 @@ public class Salesperson {
         int part_id;
         int sales_id;
 
-        System.out.println("Enter the part ID:");
+        System.out.printf("Enter the part ID:");
         Scanner scan = new Scanner(System.in);
         part_id = scan.nextInt();
-        System.out.println("Enter the Salesperson ID:");
+        System.out.printf("Enter the Salesperson ID:");
         sales_id = scan.nextInt();
 
         try {
@@ -209,12 +209,27 @@ public class Salesperson {
                     PreparedStatement pstmt_sell = conn.prepareStatement(sqlStatement_sell);
                     pstmt_sell.setInt(1, part_id);
                     pstmt_sell.executeUpdate();
+                   
+                   String sqlStatement_selectLastID = " SELECT tID FROM transaction ORDER BY tID DESC LIMIT 1 ";
+                    PreparedStatement pstmt_last = conn.prepareStatement(sqlStatement_selectLastID);
+                    ResultSet resultSet_s = pstmt_last.executeQuery();
+                    resultSet_s.next();
+                    int lastID = resultSet_s.getInt("tID");
+                    lastID = lastID + 1;
 
-                    String sqlStatement_trans= " UPDATE salesperson SET " +
-                    "salesperson.trans = salesperson.trans + 1 " +
-                    "WHERE sID = ? ";
+                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+                    LocalDate localDate = LocalDate.now();
+            
+                    String sqlStatement_trans = "INSERT INTO transaction (tID, pID, sID, tDate) " + 
+                                                " VALUES ('"+ lastID + "','" + part_id + "','" + sales_id + "','" + localDate + "') ";
+                    //System.out.println(sqlStatement_trans);                            
                     PreparedStatement pstmt_trans = conn.prepareStatement(sqlStatement_trans);
-                    pstmt_trans.setInt(1, sales_id);
+                    //@Column (name = "tDate");
+                    
+                    // pstmt_trans.setInt(1, 100);
+                    // pstmt_trans.setInt(2, part_id);
+                    // pstmt_trans.setInt(3, sales_id);
+                    // pstmt_trans.setString(4, "'" + localDate + "'" );
                     pstmt_trans.executeUpdate();
 
                     System.out.println("Product:" + pName + "(id:" + pID + ") Remaining Quality:" + pAvailableQuantity );
@@ -229,5 +244,8 @@ public class Salesperson {
             System.out.println("Error: " + exp);
         }
         scan.close();
+        System.out.println("End of Query.\n\n\n");
+        System.out.println("Return to salesperson menu.\n\n\n");
     }
+
 }
