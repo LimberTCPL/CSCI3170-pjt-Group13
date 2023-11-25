259ba6d68c2098c5c93b44350390cef2bb39da9b
LimberTCPL <139104797+LimberTCPL@users.noreply.github.com>
2023-11-25 15:54:05 +0800

    Update Salesperson_new.java

--- Salesperson_new.java
+++ Salesperson_new.java
@@ -213,13 +213,6 @@ public class Salesperson {
                     PreparedStatement pstmt_sell = conn.prepareStatement(sqlStatement_sell);
                     pstmt_sell.setInt(1, part_id);
                     pstmt_sell.executeUpdate();
-                    
-                    String sqlStatement_selectLastID = " SELECT tID FROM transaction ORDER BY tID DESC LIMIT 1 ";
-                    PreparedStatement pstmt_last = conn.prepareStatement(sqlStatement_selectLastID);
-                    ResultSet resultSet = pstmt_last.executeQuery();
-                    resultSet.next();
-                    int lastID = resultSet.getInt("tID");
-                    lastID = lastID + 1;
 
                     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
                     LocalDate localDate = LocalDate.now();
